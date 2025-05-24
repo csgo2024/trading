65ee78110c08b8331e7676134af77920169b5775
kaka <hooopeeeee@outlook.com>
2025-05-24 17:20:16 +0800

    feat: only handle telegram message from spec UserId.

--- src/Trading.Application/Telegram/HostServices/TelegramBotService.cs
+++ src/Trading.Application/Telegram/HostServices/TelegramBotService.cs
@@ -1,8 +1,10 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Trading.Common.Models;
 
 namespace Trading.Application.Telegram.HostServices;
 
@@ -11,14 +13,17 @@ public class TelegramBotService : BackgroundService
     private readonly ILogger<TelegramBotService> _logger;
     private readonly ITelegramBotClient _botClient;
     private readonly ITelegramCommandHandler _commandHandler;
+    private readonly TelegramSettings _telegramSettings;
 
     public TelegramBotService(ITelegramBotClient botClient,
                               ITelegramCommandHandler commandHandler,
+                              IOptions<TelegramSettings> telegramSettingOptions,
                               ILogger<TelegramBotService> logger)
     {
         _botClient = botClient;
         _commandHandler = commandHandler;
         _logger = logger;
+        _telegramSettings = telegramSettingOptions.Value;
     }
 
     protected override async Task ExecuteAsync(CancellationToken cancellationToken)
@@ -46,11 +51,21 @@ public class TelegramBotService : BackgroundService
         {
             if (update.CallbackQuery is { } callbackQuery)
             {
+                if (callbackQuery.From.Id != _telegramSettings.UserId)
+                {
+                    _logger.LogError("Permission denied");
+                    return;
+                }
                 await _commandHandler.HandleCallbackQuery(callbackQuery);
             }
 
             if (update.Message is { } message && message.Text is { } messageText)
             {
+                if (update.Message.From?.Id != _telegramSettings.UserId)
+                {
+                    _logger.LogError("Permission denied");
+                    return;
+                }
                 if (messageText.StartsWith('/'))
                 {
                     await _commandHandler.HandleCommand(message);
--- src/Trading.Common/Models/TelegramSettings.cs
+++ src/Trading.Common/Models/TelegramSettings.cs
@@ -4,4 +4,5 @@ public class TelegramSettings
 {
     public string? BotToken { get; set; }
     public string? ChatId { get; set; }
+    public long UserId { get; set; }
 }
